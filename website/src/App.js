9068e46197e3abde669ac8dfd5186c70419c54b2
Guang-Lin Wei <guanglinwei@gmail.com>
2023-04-06 17:21:09 -0400

    try fix gh-pages

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Outlet, useLocation, useNavigate, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Outlet, useLocation, useNavigate, HashRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TopAppBar from './components/TopAppBar';
 import AboutPage from './pages/AboutPage';
@@ -11,9 +11,9 @@ function App() {
     return (
         <div className='App'>
             TEST {process.env.PUBLIC_URL} 
-            <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+            <HashRouter>
                 <RoutesComponent />
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
@@ -29,10 +29,10 @@ function RoutesComponent() {
     const navigate = useNavigate();
 
     const { unityProvider, unload } = useUnityContext({
-        loaderUrl: './UnityBuild/Build/dist.loader.js',
-        dataUrl: './UnityBuild/Build/dist.data.unityweb',
-        frameworkUrl: './UnityBuild/Build/dist.framework.js.unityweb',
-        codeUrl: './UnityBuild/Build/dist.wasm.unityweb'
+        loaderUrl: `${process.env.PUBLIC_URL}/UnityBuild/Build/dist.loader.js`,
+        dataUrl: `${process.env.PUBLIC_URL}/UnityBuild/Build/dist.data.unityweb`,
+        frameworkUrl: `${process.env.PUBLIC_URL}/UnityBuild/Build/dist.framework.js.unityweb`,
+        codeUrl: `${process.env.PUBLIC_URL}/UnityBuild/Build/dist.wasm.unityweb`
     });
 
     async function needToUnloadCallback(path) {
