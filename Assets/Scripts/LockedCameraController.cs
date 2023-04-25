7ba3573d89ca062b834cf55818782df9b85d5189
Guang-Lin Wei <guanglinwei@gmail.com>
2023-04-24 20:32:20 -0400

    added change centered planet

--- Assets/Scripts/LockedCameraController.cs
+++ Assets/Scripts/LockedCameraController.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using TMPro;
 
 public class LockedCameraController : MonoBehaviour
 {
     public Transform target; // the planet game object that the camera will follow
     public float distance = 10f; // the distance between the camera and the planet
     public float height = 5f; // the height of the camera above the planet
-    public float rotateSpeed = 1f; // the speed of camera rotation
-    public float zoomSpeed = 20f;
+    public float rotateSpeed = 1.2f; // the speed of camera rotation
+    public float zoomSpeed = 30f;
     public float fastZoomSpeed = 200f;
 
     private Vector3 offset; // the offset of the camera from the planet
     private float currentDistance; // the current distance between the camera and the planet
     private Quaternion currentRotation; // the current rotation of the camera
+
+    private bool doLerp = false;
+    private float lerpTime = 1f;
+    private float currentLerpTime = 0f;
+    private float lerpTimeScale = 1f;
+    private Vector3 lastCameraPositionBeforeLerp;
+    private Quaternion lastCameraRotationBeforeLerp;
+
+    public TMP_InputField centerCameraOnInputField;
 
     private void Start()
     {
@@ -22,23 +32,66 @@ public class LockedCameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        // handle mouse input for camera rotation
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            float rotateHorizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-            float rotateVertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-            currentRotation *= Quaternion.Euler(-rotateVertical, rotateHorizontal, 0f);
-        }
-
-        float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
-        currentDistance = Mathf.Clamp(currentDistance - mouseScroll * (Input.GetKey(KeyCode.LeftControl) ? fastZoomSpeed : zoomSpeed), 20f, 3000f);
-
         // calculate the desired position of the camera
         Vector3 desiredPosition = target.position + currentRotation * (offset.normalized * currentDistance + Vector3.up * height);
+        
+        if (Random.Range(0f, 1f) <= 0.2f)
+        {
+            //Debug.Log(Vector3.Distance(transform.position, desiredPosition) + "  " + lerpTime);
+        }
 
-        transform.position = desiredPosition;
+        if (Vector3.Distance(transform.position, desiredPosition) <= 10f)
+        {
+            doLerp = false;
+        }
 
-        // make the camera look at the planet
-        transform.LookAt(target);
+        if (doLerp)
+        {
+            transform.position = Vector3.Lerp(lastCameraPositionBeforeLerp, desiredPosition, currentLerpTime);
+            Quaternion desiredRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(lastCameraRotationBeforeLerp, desiredRotation, currentLerpTime);
+
+            lerpTimeScale = Mathf.Max(0.2f, lerpTimeScale - Time.deltaTime * .2f);
+            currentLerpTime = Mathf.Min(1f, currentLerpTime + lerpTimeScale * Time.deltaTime / lerpTime);
+
+            
+        } else
+        {
+            // handle mouse input for camera rotation
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                float rotateHorizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+                float rotateVertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+                currentRotation *= Quaternion.Euler(-rotateVertical, rotateHorizontal, 0f);
+            }
+
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+            float currentZoomSpeed = Mathf.Max(1f, Mathf.Sqrt(currentDistance / 160f)) * (Input.GetKey(KeyCode.LeftControl) ? fastZoomSpeed : zoomSpeed);
+            currentDistance = Mathf.Clamp(currentDistance - mouseScroll * currentZoomSpeed, 20f, 3000f);
+
+            // calculate the desired position of the camera
+            desiredPosition = target.position + currentRotation * (offset.normalized * currentDistance + Vector3.up * height);
+            transform.position = desiredPosition;
+
+            // make the camera look at the planet
+            transform.LookAt(target);
+        }
+
+    }
+
+    public void OnChangeCenterObject()
+    {
+        GameObject newTarget = GameObject.Find(System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(centerCameraOnInputField.text));
+        if (newTarget != null && !newTarget.Equals(target.gameObject))
+        {
+            target = newTarget.transform;
+            float dist = Vector3.Distance(transform.position, newTarget.transform.position);
+            lerpTime = Mathf.Sqrt(dist / 3000f);
+            doLerp = true;
+            lastCameraPositionBeforeLerp = transform.position;
+            lastCameraRotationBeforeLerp = transform.rotation;
+            currentLerpTime = 0f;
+            lerpTimeScale = 1f;
+        }
     }
 }
--- website/src/pages/SimulationPage.js
+++ website/src/pages/SimulationPage.js
@@ -9,8 +9,6 @@ function SimulationPage({ unityProvider }) {
     return (
         <Box flexGrow={1} sx={{ display: 'flex', flexDirection: 'column' }}>
             <h1>Simulation</h1>
-            <p style={{ marginTop: '-20px', marginBottom: '-6px', color: 'grey' }}>Left-click + drag to rotate.
-                Scroll to zoom, hold ctrl to increase zoom speed.</p>
             <hr style={{ width: '80%' }} />
             <Box style={{ flexGrow: 1 }}>
                 {location.pathname === '/sim' ?
